257c964ea25b89c8074768702c9ff40666173b63
Hans-Georg Glöckler <h.g.gloeckler@gmx.de>
2019-03-01 14:18:16 +0100

    WithSecurityConfig

---
+++ src/main/java/org/pilger/security/SecurityConfig.java
@@ -0,0 +1,122 @@
+package org.pilger.security;
+
+import org.keycloak.adapters.KeycloakConfigResolver;
+import org.keycloak.adapters.springboot.KeycloakSpringBootConfigResolver;
+import org.keycloak.adapters.springsecurity.KeycloakSecurityComponents;
+import org.keycloak.adapters.springsecurity.authentication.KeycloakAuthenticationProvider;
+import org.keycloak.adapters.springsecurity.config.KeycloakWebSecurityConfigurerAdapter;
+import org.keycloak.adapters.springsecurity.filter.KeycloakAuthenticatedActionsFilter;
+import org.keycloak.adapters.springsecurity.filter.KeycloakAuthenticationProcessingFilter;
+import org.keycloak.adapters.springsecurity.filter.KeycloakPreAuthActionsFilter;
+import org.keycloak.adapters.springsecurity.filter.KeycloakSecurityContextRequestFilter;
+import org.keycloak.representations.AccessToken;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.FilterType;
+import org.springframework.context.annotation.Scope;
+import org.springframework.context.annotation.ScopedProxyMode;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.session.SessionRegistryImpl;
+import org.springframework.security.web.authentication.logout.LogoutFilter;
+import org.springframework.security.web.authentication.session.RegisterSessionAuthenticationStrategy;
+import org.springframework.security.web.authentication.session.SessionAuthenticationStrategy;
+import org.springframework.web.context.WebApplicationContext;
+import org.springframework.security.web.authentication.preauth.x509.X509AuthenticationFilter;
+import org.springframework.security.web.authentication.session.NullAuthenticatedSessionStrategy;
+
+/**
+ * Username: pilgeruser, Password: 1234atgfd
+ */
+@Configuration
+@ComponentScan(basePackageClasses = KeycloakSecurityComponents.class, excludeFilters = @ComponentScan.Filter(type = FilterType.REGEX, pattern = "org.keycloak.adapters.springsecurity.management.HttpSessionManager"))
+@EnableWebSecurity
+public class SecurityConfig extends KeycloakWebSecurityConfigurerAdapter {
+
+	/**
+	 * Registers the KeycloakAuthenticationProvider with the authentication manager.
+	 */
+	@Autowired
+	public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+		KeycloakAuthenticationProvider keycloakAuthenticationProvider = keycloakAuthenticationProvider();
+		auth.authenticationProvider(keycloakAuthenticationProvider);
+	}
+
+	/**
+	 * Defines the session authentication strategy.
+	 */
+	@Bean
+	@Override
+	protected SessionAuthenticationStrategy sessionAuthenticationStrategy() {
+		return new RegisterSessionAuthenticationStrategy(new SessionRegistryImpl());
+	}
+
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		super.configure(http);
+		http.addFilterBefore(keycloakPreAuthActionsFilter(), LogoutFilter.class);
+		http.sessionManagement().sessionAuthenticationStrategy(sessionAuthenticationStrategy());
+		http.authorizeRequests().antMatchers("/VAADIN/**", "/favicon.ico", "/robots.txt").permitAll()
+				.antMatchers("/manifest.webmanifest", "/sw.js", "/offline-page.html").permitAll()
+				.antMatchers("/icons/**", "/images/**", "/frontend/**", "/webjars/**").permitAll()
+				.antMatchers("/h2-console/**", "/frontend-es5/**", "/frontend-es6/**").permitAll().anyRequest()
+				.authenticated();
+		// Disable CSRF (cross site request forgery)
+		http.csrf().disable();
+
+		// Disable page caching
+		http.headers().cacheControl();
+	}
+
+	/**
+	 * Look for Config not in applicatoin.propertie, not in keycloak.json
+	 */
+	@Bean
+	public KeycloakConfigResolver KeycloakConfigResolver() {
+		return new KeycloakSpringBootConfigResolver();
+	}
+
+	@Bean
+	public FilterRegistrationBean keycloakAuthenticationProcessingFilterRegistrationBean(
+			KeycloakAuthenticationProcessingFilter filter) {
+		FilterRegistrationBean registrationBean = new FilterRegistrationBean(filter);
+		registrationBean.setEnabled(false);
+		return registrationBean;
+	}
+
+	@Bean
+	public FilterRegistrationBean keycloakPreAuthActionsFilterRegistrationBean(KeycloakPreAuthActionsFilter filter) {
+		FilterRegistrationBean registrationBean = new FilterRegistrationBean(filter);
+		registrationBean.setEnabled(false);
+		return registrationBean;
+	}
+
+	@Bean
+	public FilterRegistrationBean keycloakAuthenticatedActionsFilterBean(KeycloakAuthenticatedActionsFilter filter) {
+		FilterRegistrationBean registrationBean = new FilterRegistrationBean(filter);
+		registrationBean.setEnabled(true);
+		return registrationBean;
+	}
+
+	@Bean
+	public FilterRegistrationBean keycloakSecurityContextRequestFilterBean(
+			KeycloakSecurityContextRequestFilter filter) {
+		FilterRegistrationBean registrationBean = new FilterRegistrationBean(filter);
+		registrationBean.setEnabled(false);
+		return registrationBean;
+	}
+
+	@Bean
+	@Scope(scopeName = WebApplicationContext.SCOPE_REQUEST, proxyMode = ScopedProxyMode.TARGET_CLASS)
+	public AccessToken getAccessToken() {
+		HttpServletRequest request = ((ServletRequestAttributes) RequestContextHolder.currentRequestAttributes())
+				.getRequest();
+		return ((KeycloakPrincipal) request.getUserPrincipal()).getKeycloakSecurityContext().getToken();
+	}
+
+}
